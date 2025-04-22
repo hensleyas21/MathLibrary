e6a54458f773461973183dc45dc652e35843fffa
hensleyas21 <hensleyas21@gcc.edu>
2025-04-22 16:37:26 -0400

    Vector class

---
+++ src/main/scala/Vector.scala
@@ -0,0 +1,35 @@
+import scala.annotation.targetName
+
+class Vector(val data: List[Double]) {
+
+  def size: Int = data.length
+
+  def magnitude: Double = L2Norm
+
+  @targetName("add")
+  def +(other: Vector): Vector = {
+    if size != other.size then throw new UnsupportedOperationException("Vectors must be the same size") else
+    new Vector(data.zip(other.data).map { case (a, b) => a + b })
+  }
+
+  @targetName("scale")
+  def *(scalar: Double): Vector = {
+    new Vector(data.map(x => x * scalar))
+  }
+
+  def L1Norm: Double = data.map(x => math.abs(x)).sum
+
+  def L2Norm: Double = math.sqrt(data.map(x => x * x).sum)
+
+  def maximumNorm: Double = data.map(x => math.abs(x)).max
+
+  def dotProduct(other: Vector): Double = {
+    if size != other.size then throw new UnsupportedOperationException("Vectors must be the same size") else
+    data.zip(other.data).map { case (a, b) => a * b }.sum
+  }
+
+  @TODO
+  def crossProduct(otherVectors: List[Vector]): Vector = {
+    new Vector([0])
+  }
+}
