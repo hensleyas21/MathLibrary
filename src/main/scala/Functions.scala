f184f4282adf3787fac0a83e6591758109b120c9
pickleroot <pickleroot@protonmail.com>
2025-04-30 15:53:01 -0400

    stub for MarkovChain

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -15,3 +15,16 @@ def MiddleRiemannSum(f: Double => Double, a:Double, b:Double, n:Int): Double = {
   val sum = (0 until n).map(i => f(a + (i+0.5)*deltaX)).sum
   sum * deltaX
 }
+
+/**
+ * Computes the Markov chain for a specified length n and returns the resulting vector
+ * @param vector - the vector to start with
+ * @param matrix - the stochastic matrix
+ * @param n - the number of times to multiply the vector by the matrix
+ * @return - vector: MyVector
+ */
+def MarkovChain(vector: MyVector, matrix: Matrix, n: Int): MyVector = {
+  // need to parallelize the multiplication of matrices (they can be multiplied in any order (kinda))
+  // before multiplying the vector by it
+  vector
+}
