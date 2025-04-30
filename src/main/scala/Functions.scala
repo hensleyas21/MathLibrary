54f86bf53804b9582c4a3c668c9b803689bbaecf
Kesterholtem21 <kesterholtem21@gcc.edu>
2025-04-30 15:50:00 -0400

    parallelize reimann sums

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -15,3 +15,21 @@ def MiddleRiemannSum(f: Double => Double, a:Double, b:Double, n:Int): Double = {
   val sum = (0 until n).map(i => f(a + (i+0.5)*deltaX)).sum
   sum * deltaX
 }
+
+def LeftRiemannSumParallel(f: Double => Double, a:Double, b:Double, n:Int): Double = {
+  val deltaX = (b - a)/n
+  val sum = (0 until n).par.map(i => f(a + i*deltaX)).sum
+  sum * deltaX
+}
+
+def RightRiemannSumParallel(f: Double => Double, a:Double, b:Double, n:Int): Double = {
+  val deltaX = (b - a)/n
+  val sum = (0 until n).par.map(i => f(a + (i+1)*deltaX)).sum
+  sum * deltaX
+}
+
+def MiddleRiemannSumParallel(f: Double => Double, a:Double, b:Double, n:Int): Double = {
+  val deltaX = (b - a)/n
+  val sum = (0 until n).par.map(i => f(a + (i+0.5)*deltaX)).sum
+  sum * deltaX
+}
