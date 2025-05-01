36c96109a2c28586b12c8d88d24e2c2bb8f23831
hensleyas21 <hensleyas21@gcc.edu>
2025-05-01 08:58:22 -0400

    Added par import

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -1,3 +1,5 @@
+import scala.collection.parallel.CollectionConverters._
+
 def LeftRiemannSum(f: Double => Double, a:Double, b:Double, n:Int): Double = {
   val deltaX = (b - a)/n
   val sum = (0 until n).map(i => f(a + i*deltaX)).sum
